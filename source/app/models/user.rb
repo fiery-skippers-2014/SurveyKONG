48dad910e908aed7aca037d906fb5fad472c818b
Greg Gerla & Paolo Payumo <pair+gregorygerla.paopay@devbootcamp.com>
2014-04-11 13:55:21 -0700

    added self.autheticate in user model

--- source/app/models/user.rb
+++ source/app/models/user.rb
@@ -6,4 +6,13 @@ class User < ActiveRecord::Base
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
   validates_presence_of :name
 
+  def self.authenticate(email, password)
+    @user = User.find_by_email(email)
+    if @user && @user.password == password
+      return @user
+    else
+      return nil
+    end
+  end
+
 end
