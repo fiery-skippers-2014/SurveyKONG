9f94ed2128f874e4483459cedcd48ee61a9bdd18
& <pair+mattlewissf.paopay@devbootcamp.com>
2014-04-11 16:26:00 -0700

    basic jquery form

--- source/public/js/application.js
+++ source/public/js/application.js
@@ -1,68 +1,17 @@
 $(document).ready(function () {
 
-$(function() {
-var addDiv = $('#addinput');
-var i = $('#addinput p').size() + 1;
-$('#addNew').live('click', function() {
-$('<p><input type="text" id="p_new" size="40" name="p_new_' + i +'" value="" placeholder="I am New" /><a href="#" id="remNew">Remove</a> </p>').appendTo(addDiv);
-i++;
 
-return false;
 
-});
-$('#remNew').live('click', function() {
 
-if( i > 2 ) {
+  var counter = 0;
 
-$(this).parents('p').remove();
-
-i--;
-}
-return false;
-});
-});
-
+  $('.add').on('click', function(){
+      counter ++;
+      $('<input id="question'+counter+'" class="add">Add</input><a href="#">Click</a>').insertBefore('.add:last');
+  });
 });
 
 
 
 
 
-
-
-//   // Optional - Use AJAX to send an HTTP DELETE request for the sign-out link
-//   $('.postvote').on('click', function(event) {
-//     event.preventDefault();
-//     var go = $(this).parent();
-
-//     $.ajax({
-//         type: "post",
-//         url: "/postvote",
-//         data: $(go).serialize(),
-//         success: function(data) {
-//           var json = JSON.parse(data)
-//           $('#upvotes'+json.post_id).html(json.votecount)
-//         },
-//         error: function() {
-//           console.log("fail")
-//         }
-//     });
-//   });
-
-//   $('.postcomment').on('click', function(event) {
-//       event.preventDefault();
-//       var go = $(this).parent();
-
-
-//     $.ajax({
-//       type: "post",
-//       url: "/postcomment",
-//       data: $(go).serialize(),
-//       success: function(data) {
-//         var json = JSON.parse(data);
-//         console.log(json)
-//         $('#upvotesa'+json.comment_id).html(json.votecount)
-//       }
-//     });
-//   });
-// });
